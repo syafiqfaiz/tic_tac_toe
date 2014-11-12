860e600a33bfb4f12bdf0b3c7dd2609542bf3da7
Code Division <hello@11th.ventures>
2014-11-12 15:31:30 +0800

    starting to get realtime multiplayer to work

--- app/controllers/games.rb
+++ app/controllers/games.rb
@@ -2,5 +2,18 @@ enable :sessions
 
 
 get '/game/lobby' do
+  @games = Game.where(player_2: nil)
+  erb :'/game/lobby'
+end
+
+get '/game/create' do
+  current_game = Game.create(player_1: session[:id])
+  erb :'/game/wait_player'
+end
+
+get '/game/:game_id/:player_2_id' do
+  current_game = Game.find(params[:game_id])
+  current_game.player_2 = params[:player_2_id]
+  current_game.save
   erb :'/game/play'
 end
